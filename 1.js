6a18f2d0f5a25c647d97b9da7f834a3ce39c6739
unknown <ngodacquoclong@gmail.com>
2020-10-20 20:51:35 +0700

    css for index

---
+++ 1.js
@@ -0,0 +1,30 @@
+var questionQuiz = [
+  { question: 'Bác Hồ sinh năm bao nhiêu ?',
+    answer_a: '1900',
+    answer_b: '1911',
+    answer_c: '1890',
+    answer_d: '1891',
+    correct: 'c'
+  },
+  { question: '1 + 1 = ?',
+    answer_a: '4',
+    answer_b: '2',
+    answer_c: '5',
+    answer_d: '6',
+    correct: 'b'
+  },
+  { question: '2 + 2 = ?',
+    answer_a: '4',
+    answer_b: '2',
+    answer_c: '5',
+    answer_d: '6',
+    correct: 'a'
+  },
+  { question: '2 * 3 = ?',
+    answer_a: '4',
+    answer_b: '2',
+    answer_c: '5',
+    answer_d: '6',
+    correct: 'd'
+  }
+]
